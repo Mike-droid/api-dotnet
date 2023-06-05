37aad21833605440d4041ac5e1b4fb1c9551b909
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-06-05 11:23:11 -0500

    Clase 8 - atributos para verbos HTTP

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,20 +13,45 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    private static List<WeatherForecast> listWeatherForecast = new List<WeatherForecast>();
+
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        if (listWeatherForecast == null || !listWeatherForecast.Any())
+        {
+            listWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return listWeatherForecast;
+    }
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {
+        listWeatherForecast.Add(weatherForecast);
+        return Ok("Added weather forecast to list");
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index)
+    {
+        if (index < 0 || index >= listWeatherForecast.Count)
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            return BadRequest("Index out of bounds");
+        }
+        listWeatherForecast.RemoveAt(index);
+        return Ok("Deleted weather forecast from list");
     }
 }
