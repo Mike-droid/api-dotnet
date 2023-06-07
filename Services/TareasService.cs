fb20160be6e5226a4f85c21753fb81d71abae063
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-06-07 15:44:49 -0500

    Clase 18 - creacion de servicios

---
+++ Services/TareasService.cs
@@ -0,0 +1,53 @@
+using webapi.Models;
+namespace webapi.Services;
+
+public class TareasService : ITareasService
+{
+  TareasContext context;
+
+  public TareasService(TareasContext dbContext)
+  {
+    context = dbContext;
+  }
+
+  public IEnumerable<Tarea> Get()
+  {
+    return context.Tareas;
+  }
+
+  public async Task Save(Tarea tarea)
+  {
+    context.Tareas.Add(tarea);
+    await context.SaveChangesAsync();
+  }
+
+  public async Task Update(Guid id, Tarea tarea)
+  {
+    var tareaActual = context.Tareas.Find(id);
+    if (tareaActual != null)
+    {
+      tareaActual.Titulo = tarea.Titulo;
+      tareaActual.Descripcion = tarea.Descripcion;
+      tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+      tareaActual.FechaCreacion = tarea.FechaCreacion;
+      tareaActual.Categoria = tarea.Categoria;
+
+      await context.SaveChangesAsync();
+    }
+  }
+
+  public async Task Delete(Guid id)
+  {
+    var tareaActual = context.Tareas.Find(id);
+    if (tareaActual != null)
+    {
+      context.Tareas.Remove(tareaActual);
+      await context.SaveChangesAsync();
+    }
+  }
+}
+
+public interface ITareasService
+{
+  IEnumerable<Tarea> Get();
+}
