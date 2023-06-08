629dee8b941e91a2de50b6a1d6474ea5717a3304
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-06-07 20:35:06 -0500

    Clase 21 - probando API con BBDD

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -8,12 +8,15 @@ public class HelloWorldController:  ControllerBase
 {
     IHelloWorldService helloWorldService;
 
+    TareasContext dbContext;
+
     private readonly ILogger<HelloWorldController> _logger;
 
-    public HelloWorldController(IHelloWorldService helloWorld, ILogger<HelloWorldController> logger)
+    public HelloWorldController(IHelloWorldService helloWorld, ILogger<HelloWorldController> logger, TareasContext db)
     {
         helloWorldService = helloWorld;
         _logger = logger;
+        dbContext = db;
     }
 
     [HttpGet]
@@ -21,5 +24,13 @@ public class HelloWorldController:  ControllerBase
     {
         _logger.LogInformation("This is the log for hello world!");
         return Ok(helloWorldService.GetHelloWorld());
+    }
+
+    [HttpGet]
+    [Route("createdb")]
+    public IActionResult CreateDatabase()
+    {
+        dbContext.Database.EnsureCreated();
+        return Ok("Database created successfully!");
     }
 }
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,6 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace webapi.Models;
 
 public class Tarea
